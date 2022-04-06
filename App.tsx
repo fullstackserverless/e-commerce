5b38cb102ce9219101a8d95172eeb1d721635914
katsuhira02 <97621153+katsuhira02@users.noreply.github.com>
2022-04-06 20:27:03 +0500

    add components buttons and space (#61)
    Co-authored-by: Danil <rosesmaster2007@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,8 +1,36 @@
 import * as React from 'react'
 import { Text } from 'react-native'
+import { StyleSheet, ScrollView, View } from 'react-native'
+import { DARK, WHITE } from './src/constants'
+import { Button, Space } from './src/components'
 
-const App = () => (
-  <Text>Hello world!</Text>
-)
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: DARK
+  },
+  textStyle: {
+    color: WHITE,
+    fontSize: 25,
+    paddingBottom: 10
+  }
+})
 
-export default App
+export default function App({}){
+  const { container, textStyle } = styles
+  return(
+    <View style={container}>
+      <Text style={textStyle}>Buttons</Text>
+      <Button isOutline={true} isSmall={false} title='Push me'/>
+      <Space height={15} />
+      <Button isOutline={false} isSmall={false} title='Push me'/>
+      <Space height={15} />
+      <Button isOutline={true} isSmall={true} title='Push me'/>
+      <Space height={15} />
+      <Button isOutline={false} isSmall={true} title='Push me'/>
+      <Space height={15} />
+    </View>
+  )
+}
