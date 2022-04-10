4cbe5bbd7ea7751ae84425718b5de643a02f08da
Evgeniy Kireev <jocodersapp@gmail.com>
2022-04-10 17:06:21 +0300

    added svg library

---
+++ src/components/CheckBox/index.tsx
@@ -0,0 +1,12 @@
+import React, { memo } from 'react'
+import { View, ViewProps } from 'react-native'
+
+type CheckBoxType = 'active' | 'inActive'
+
+export interface CheckBoxProps {
+    isActive: boolean;
+}
+
+const CheckBox = () => {
+
+}
