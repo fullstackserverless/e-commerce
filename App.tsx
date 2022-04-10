b02582a0f72d8ae754545343ca6845285ba0c411
Evgeniy Kireev <jocodersapp@gmail.com>
2022-04-10 18:44:07 +0300

    added check icon, checkbox component

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import { Text } from 'react-native'
-import { StyleSheet, ScrollView, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { DARK, WHITE } from './src/constants'
-import { Button, Space, Switch } from './src/components'
+import { Button, Space, Switch, CheckBox } from './src/components'
 
 const styles = StyleSheet.create({
   container: {
@@ -21,11 +21,16 @@ const styles = StyleSheet.create({
 export default function App({}){
   const { container, textStyle } = styles
   const [isEnabled, setIsEnabled] = useState(false)
+  const [redCheckBoxValue, setRedCheckBoxValue] = useState(false)
+  const [whiteCheckBoxValue, setWhiteCheckBoxValue] = useState(false)
+
   const toggleSwitch = () => setIsEnabled(previousState => !previousState)
+  const toggleRedCheckBox = () => setRedCheckBoxValue(redCheckBoxValue => !redCheckBoxValue)
+  const toggleWhiteCheckBox = () => setWhiteCheckBoxValue(whiteCheckBoxValue => !whiteCheckBoxValue)
 
   return(
     <View style={container}>
-      <Text style={textStyle}>Buttons</Text>
+      <Text style={textStyle}>{'Buttons'}</Text>
       <Button isOutline={true} isSmall={false} title='Push me'/>
       <Space height={15} />
       <Button isOutline={false} isSmall={false} title='Push me'/>
@@ -34,10 +39,15 @@ export default function App({}){
       <Space height={15} />
       <Button isOutline={false} isSmall={true} title='Push me'/>
       <Space height={30} />
-      <Text style={textStyle}>Switch</Text>
+      <Text style={textStyle}>{'Switch'}</Text>
       <Space height={15} />
       <Switch isValue={isEnabled} onValueChange={toggleSwitch}/>
       <Space height={30} />
+      <Text style={textStyle}>{'CheckBox'}</Text>
+      <Space height={30} />
+      <CheckBox activeColor={'primary'} value={redCheckBoxValue} onToggle={toggleRedCheckBox} />
+      <Space height={30} />
+      <CheckBox activeColor={'white'} value={whiteCheckBoxValue} onToggle={toggleWhiteCheckBox} />
     </View>
   )
 }
--- src/components/CheckBox/index.tsx
+++ src/components/CheckBox/index.tsx
@@ -1,12 +1,69 @@
-import React, { memo } from 'react'
-import { View, ViewProps } from 'react-native'
-
-type CheckBoxType = 'active' | 'inActive'
+import React, {FC, useEffect, useMemo, useState} from 'react'
+import {StyleSheet, Animated, Easing} from 'react-native'
+import {Check} from '../../icons'
+import {COLORS} from "../../theme";
+import {TouchableOpacity} from "../TouchableOpacity";
 
 export interface CheckBoxProps {
-    isActive: boolean;
+    value: boolean
+    activeColor: 'primary' | 'white'
+    onToggle: (value: boolean) => void
 }
 
-const CheckBox = () => {
+const animationToggle = (value: boolean) => new Animated.Value(value ? 1 : 0)
 
+export const CheckBox: FC<CheckBoxProps> = (props: CheckBoxProps) => {
+    const {value, onToggle, activeColor} = props
+    const [timer] = useState<Animated.Value>(animationToggle(value))
+    const [previousValue, setPreviousValue] = useState<boolean>(value)
+    const onToggleHandler = React.useMemo(() => () => onToggle && onToggle(!value), [onToggle, value])
+
+    const startAnimation = useMemo(() => (value: boolean) => {
+        Animated.timing(timer, {
+            toValue: value ? 1 : 0,
+            duration: 250,
+            easing: Easing.out(Easing.circle),
+            useNativeDriver: false,
+        }).start()
+    }, [timer])
+
+    useEffect(() => {
+        if (value !== previousValue) {
+            startAnimation(value)
+            setPreviousValue(value)
+        }
+    }, [value, previousValue, startAnimation, setPreviousValue])
+
+    const animatedBgStyle = timer.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['transparent', COLORS[activeColor]]
+    })
+    const animatedBorderColorStyle = timer.interpolate({
+        inputRange: [0, 1],
+        outputRange: [COLORS.gray, 'transparent']
+    })
+    const borderWidth = value ? 0 : 1
+    const iconColor = !value ? 'transparent' : activeColor === COLORS.primary ? COLORS.white : COLORS.dark
+
+    return (
+        <TouchableOpacity onPress={onToggleHandler}>
+            <Animated.View style={[styles.container, {
+                backgroundColor: animatedBgStyle,
+                borderColor: animatedBorderColorStyle,
+                borderWidth,
+            }]}>
+                <Check color={iconColor}/>
+            </Animated.View>
+        </TouchableOpacity>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width: 20,
+        height: 20,
+        borderRadius: 4,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
