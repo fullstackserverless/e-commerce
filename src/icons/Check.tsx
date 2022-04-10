9a23935ec9804d3821b0ec6e838287b53fd85757
Evgeniy Kireev <jocodersapp@gmail.com>
2022-04-10 17:08:00 +0300

    added svg library

--- src/icons/Check.tsx
+++ src/icons/Check.tsx
@@ -6,8 +6,7 @@ const SvgComponent = (props: SvgProps) => (
         width={15}
         height={12}
         fill="none"
-        {...props}
-    >
+        {...props}>
         <Path
             d="M4.767 9.468 1.21 5.736 0 6.998 4.767 12 15 1.262 13.798 0 4.767 9.468Z"
             fill="#F7F7F7"
